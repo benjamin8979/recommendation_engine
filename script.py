1eeba2da8d2db1529c479bf998674cb79fe2ce0e
codecademy <ccuser@codecademy.com>
2020-08-04 01:50:09 +0000

    Created attractions and functionality for adding new attractions

--- script.py
+++ script.py
@@ -13,6 +13,17 @@ def get_traveler_location(traveler):
 
 test_destination_index = get_traveler_location(test_traveler)
 
-print(test_destination_index)
+attractions = [[] for lst in destinations]
 
+def add_attraction(destination, attraction):
+  try:
+    destination_index = get_destination_index(destination)
+  except ValueError:
+    return
+  attractions_for_destination = attractions[destination_index]
+  attractions_for_destination.append(attraction)
+  return
 
+add_attraction("Los Angeles, USA", ['Venice beach', ['beach']])
+
+print(attractions)
